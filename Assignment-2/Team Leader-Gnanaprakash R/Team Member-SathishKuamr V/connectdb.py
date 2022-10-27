12d03c12644e739c78fa19e270ab3aebb450f465
sathish140602 <111893172+sathish140602@users.noreply.github.com>
2022-10-27 15:00:26 +0530

    Delete Assignment-2/Team Leader-Gnanaprakash R/Team Member-SathishKuamr V directory

--- Assignment-2/Team Leader-Gnanaprakash R/Team Member-SathishKuamr V/connectdb.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('students.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE students (name TEXT,contact TEXT)')
-print("Table created successfully")
-conn.close()
